4212a8b872a3a6c657bfef189fcb7d5d11e46a25
Elyesrafa <elyesarfa7211@gmail.com>
2024-03-20 23:32:12 +0100

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""base class for console"""
 
 import uuid
 from datetime import datetime
-from models import storage
-
 
 class BaseModel:
     def __init__(self, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if (kwargs is not None):
+        if kwargs is not None:
             format = "%Y-%m-%dT%H:%M:%S.%f"
             try:
                 self.id = kwargs["id"]
@@ -21,13 +18,13 @@ class BaseModel:
                 self.updated_at = y
             except KeyError:
                 pass
-            storage.new(self)
 
     def __str__(self):
-        return str(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
+        from models import storage
         storage.save()
 
     def to_dict(self):
